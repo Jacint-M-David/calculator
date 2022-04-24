019136fc0c33fb167f3ca0d82d67976ae106f40e
Jacint M. David <jacint.m.david@gmail.com>
2022-04-25 00:10:26 +0100

    Completed up until User Story #6

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-    
+
   return (
   <div className="App">
       <div id="calculator">
-          <div id="dislay">
+          <div id="display">
               <output id="partial"></output>
-              <output id="total"></output>
+              <output id="total">0</output>
           </div>
           <div id="buttons">
               <button id="equals">=</button>
-              <div id="numbers">
-                <button id="zero">0</button>
-                <button id="one">1</button>
-                <button id="two">2</button>
-                <button id="three">3</button>
-                <button id="four">4</button>
-                <button id="five">5</button>
-                <button id="six">6</button>
-                <button id="seven">7</button>
-                <button id="eight">8</button>
-                <button id="nine">9</button>
-              </div>
-              <div id="operators">
-                <button id="add">+</button>
-                <button id="subtract">-</button>
-                <button id="multiply">*</button>
-                <button id="divide">/</button>
-              </div>
+                <button className="number-buttons" id="zero">0</button>
+                <button className="number-buttons" id="one">1</button>
+                <button className="number-buttons" id="two">2</button>
+                <button className="number-buttons" id="three">3</button>
+                <button className="number-buttons" id="four">4</button>
+                <button className="number-buttons" id="five">5</button>
+                <button className="number-buttons" id="six">6</button>
+                <button className="number-buttons" id="seven">7</button>
+                <button className="number-buttons" id="eight">8</button>
+                <button className="number-buttons" id="nine">9</button>
+                <button className="operator-buttons" id="add">+</button>
+                <button className="operator-buttons" id="subtract">-</button>
+                <button className="operator-buttons" id="multiply">x</button>
+                <button className="operator-buttons" id="divide">/</button>
               <button id="decimal">.</button>
               <button id="clear">AC</button>
 
