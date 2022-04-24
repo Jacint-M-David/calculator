fe3619286e12278741bc5423e82662efb8e96c56
Jacint M. David <jacint.m.david@gmail.com>
2022-04-24 19:35:27 +0100

    Initial Commit with basic ui frame

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className="App">
+      <div id="calculator">
+          <div id="dislay">
+              <output id="partial"></output>
+              <output id="total"></output>
+          </div>
+          <div id="buttons">
+              <button id="equals">=</button>
+              <div id="numbers">
+                <button id="zero">0</button>
+                <button id="one">1</button>
+                <button id="two">2</button>
+                <button id="three">3</button>
+                <button id="four">4</button>
+                <button id="five">5</button>
+                <button id="six">6</button>
+                <button id="seven">7</button>
+                <button id="eight">8</button>
+                <button id="nine">9</button>
+              </div>
+              <div id="operators">
+                <button id="add">+</button>
+                <button id="subtract">-</button>
+                <button id="multiply">*</button>
+                <button id="divide">/</button>
+              </div>
+              <button id="decimal">.</button>
+              <button id="clear">AC</button>
+
+          </div>
+      </div>
+  </div>
+  )
 }
 
 export default App;
